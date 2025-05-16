a6f456bbba0e3879b6867d58406ca481e154e404
Michaela <mikaelamcrowdie@gmail.com>
2025-05-16 12:59:43 +0200

    Updated folder structure

---
+++ src/js/quiz_app.js
@@ -0,0 +1,8 @@
+class QuizApp {
+  constructor() {
+    this.currentQuestionIndex = 0;
+    this.score = 0;
+    this.questions = [];
+    this.container = document.getElementById(containerId);
+  }
+}
